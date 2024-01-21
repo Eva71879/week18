331bf68b6c262846eab36191a346558ef0f14ef6
Elena <Eva71879@gmail.com>
2024-01-21 23:43:21 +0300

    done second task

--- task2.js
+++ task2.js
@@ -1,21 +1,56 @@
-
 const input = document.querySelector('.addTask__input');
 const taskEmpty = document.querySelector('.task__empty');
 const errorTask = document.querySelector('.errorTask');
 const clearButton = document.querySelector('.clearButton');
 const taskList = document.querySelector('.task');
+let taskListItems = localStorage.getItem('taskListItems');
+
+if (taskListItems) {
+    clearButton.disabled = false; //я помню рекомендацию не делать disabled кнопки изначально, но здесь в задании прямо указано сделать кнопку неактивной
+    taskEmpty.style.display = "none";
+    clearButton.classList.add("enabled");
+}
+else {
+    clearButton.disabled = true;
+    taskEmpty.style.display = "block";
+}
+
+const loadFromStorage = () => {
+    taskListItems = taskListItems ? JSON.parse(taskListItems) : [];
+    for (let i = 0; i < taskListItems.length; i++) {
+            taskList.innerHTML +=
+            `<li>
+            <label class="label">
+              <input
+                type="checkbox"
+                name="checkbox-name"
+                class="real-checkbox"
+              />${taskListItems[i]}
+              <span class="custom-checkbox"></span>
+            </label>
+          </li>`;
+    }
+}
+loadFromStorage ();
 
 function addTask () {
     if (input.value.trim().length) {
-        taskEmpty.style.opacity = 0;
+        taskEmpty.style.display = "none";
         errorTask.innerText = '';
         input.style.border = '1px solid #000000'
         taskList.innerHTML +=
-        `<li class="task__item">
-          <input type="checkbox" class="task__item" />${input.value}
-        </li>`;
-        clearButton.disabled = false; //я помню рекомендацию не делать disabled кнопки изначально, но здесь в задании прямо указано сделать кнопку неактивной
-
+        `          <li>
+            <label class="label">
+              <input
+                type="checkbox"
+                name="checkbox-name"
+                class="real-checkbox"
+              />${input.value}
+              <span class="custom-checkbox"></span>
+            </label>
+          </li>`;
+        clearButton.disabled = false;
+        clearButton.classList.add("enabled");
         addtoStorage();
     }
     else {
@@ -27,38 +62,20 @@ document.querySelector('.addTask__button').onclick = addTask;
 
 const addtoStorage = () => {
     let taskListItems = localStorage.getItem('taskListItems');
-    taskListItems = taskListItems ? JSON.parse(taskListItems) : [];
-    
+    taskListItems = taskListItems ? JSON.parse(taskListItems) : [];    
     taskListItems.push(input.value);
     localStorage.setItem('taskListItems', JSON.stringify(taskListItems));
     input.value = '';
-    console.log(`Задача ${input.value} добавлена в список и сохранена в Local Storage`);
 }
-
-const loadFromStorage = () => {
-    let taskListItems = localStorage.getItem('taskListItems');
-    taskListItems = taskListItems ? JSON.parse(taskListItems) : [];
-    for (let i = 0; i < taskListItems.length; i++) {
-        const object = taskListItems[i];
-        for (let key in object) {
-            taskList.innerHTML +=
-            `<li class="task__item">
-              <input type="checkbox" class="task__item" />${object[key]}
-            </li>`;
-        }
-    }
-}
-loadFromStorage ();
-
-  
-
 
 function clearList () {
     taskList.innerHTML = '';
-    taskEmpty.style.opacity = 1;
+    localStorage.clear();
+    clearButton.disabled = true;
+    clearButton.classList.remove("enabled");
+    taskEmpty.style.display = "block";
 }
 clearButton.onclick = clearList;
-
 
 
 // function checkTask(evt) {
@@ -67,10 +84,3 @@ clearButton.onclick = clearList;
 //     }
 //    }
 //    document.querySelector('.task').onclick = checkTask;
-
-
-// - Каждый чекбокс напротив задачи должен менять своё состояние при клике (говоря нам, что задача выполнена)+
-// - При клике на кнопку «Очистить список» все задачи должны удаляться+
-    
-//     **Важно: Для сохранения состояния списка задач между сеансами работы с приложением используйте Local Storage. Это позволит восстановить список задач при повторном открытии приложения.**
-    
